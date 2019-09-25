7256821c2b08933fbbf5fd45129ddec395fc1e74
POOJAKGUPTA <POOJAKGUPTA4@GMAIL.com>
2019-09-25 20:11:00 +0530

    new api add

--- src/view/create.component.js
+++ src/view/create.component.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
+const $ = window.$;
 export default class Create extends Component {
+     handleClick() {
+    var name = this.refs.name.value;
+    console.log("this.ref.name.value")
+    // var description = this.refs.description.value;
+    $.ajax({
+      url: "http://localhost:5000/blogs",
+      type: "POST",
+      data: { blog: { name: name } },
+      success: response => {
+        console.log("it worked!", response);
+      }
+    });
+  }
     render() {
         return (
             <div style={{marginTop: 10}}>
@@ -9,21 +23,23 @@ export default class Create extends Component {
                 <form>
                     <div className="form-group">
                         <label>Add Person Name:  </label>
-                        <input type="text" className="form-control"/>
+                        <input ref="name" className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label>Add Business Name: </label>
-                        <input type="text" className="form-control"/>
+                        <input ref="business" className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label>Add GST Number: </label>
-                        <input type="text" className="form-control"/>
+                        <input ref="gstno" className="form-control"/>
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Register Business" className="btn btn-primary"/>
+                        <input ref="submit" value="Register Business" className="btn btn-primary"/>
+                         <button onClick={this.handleClick}>Submit</button>
                     </div>
                 </form>
             </div>
         )
     }
 }
+
