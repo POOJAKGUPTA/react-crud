b35a6d8115ee0cb6acc21087e62fb08a89fc6aec
POOJAKGUPTA <POOJAKGUPTA4@GMAIL.com>
2019-09-30 13:21:01 +0530

    post api json.typicode

--- src/view/create.component.js
+++ src/view/create.component.js
@@ -1,48 +1,56 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 class Create extends Component {
   constructor(props){
     super(props)
       this.state = {
         name: '',
-        business_nbame: '',
-        gst_no:  '',
+        title: '',
+        body:  ''
     }
-
   }
-   changeHandler = (e) => {
-        this.setState({[e.target.name]: e.target.vale})
+    changeHandler = e => {
+        this.setState({ [e.target.name] : e.target.value})
       }
-    submitHandler = (e) => {
+    submitHandler = e => {
       e.preventDefault()
       console.log(this.state)
+      axios.post('http://jsonplaceholder.typicode.com/posts', this.state)
+      .then( response => {
+        console.log(response)
+      })
+    .catch(error =>{
+        console.log(error)
+    })
     }
     render() {
-      const { name, business_nbame, gst_no } = this.state
+      const { userId, title, body } = this.state
         return (
             <div style={{marginTop: 10}}>
             	<p>Welcome to Create Component!!</p>
                 <h3>Add New Business</h3>
-                <form  onSubmit={this.submitHandler} >
-                    <div className="form-group">
+                <form onSubmit={this.submitHandler}>
+                     <div className="form-group">
                         <label>Add Person Name:  </label>
-                        <input type="text" name="name" value = {name} onChange={this.changeHandler} className="form-control"/>
+                        <input type="text" name="userId" value={userId} onChange={this.changeHandler} className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label>Add Business Name: </label>
-                        <input type="text"  name="business_nbame" value = {business_nbame} onChange={this.changeHandler} className="form-control"/>
+                        <input type="text"  name="title" value={title} onChange={this.changeHandler} className="form-control"/>
                     </div>
                     <div className="form-group">
                         <label>Add GST Number: </label>
-                        <input type="text"  name="gst_no" value = {gst_no} onChange={this.changeHandler} className="form-control"/>
+                        <input type="text"  name="body" value={body} onChange={this.changeHandler} className="form-control"/>
                     </div>
                     <div className="form-group">
-                        <button type="submit" value="Register Business" className="btn btn-primary"/>
+                    <button type="submit" className="btn btn-primary">Register Business</button>
                     </div>
                 </form>
             </div>
         )
     }
 }
- export default Create;
+export default Create;
+
+
 
